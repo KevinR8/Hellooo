2ab4edf92f7e4cf6c6e2f62f1aec106ce7a8a4cb
KevinR8 <kevinaramos2002@gmail.com>
2021-06-23 09:26:01 -0700

    made into html

--- caloriecalculator/calculator.php
+++ caloriecalculator/calculator.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 
 $sex = $_GET['sex'];
@@ -19,7 +28,7 @@ if ($training = 0) {
     $intensity = 1.725;
 }
 
-if ($sex = 'male') [
+if ($sex = 'male') {
     $BMR = 88.362 + (13.397 * $weight) + (4.799 * $height) + (5.677 * $age);
     $calories = $BMR * $intensity;
 } elseif ($sex = 'female') {
@@ -32,3 +41,5 @@ echo "you should be eating " , $calories , "a day";
 
 
 ?>
+</body>
+</html>
