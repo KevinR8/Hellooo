2758b317dd6e98f85a81157550b195f123cfea1c
KevinR8 <80863717+KevinR8@users.noreply.github.com>
2021-08-05 18:59:11 -0700

    Mergin

---
+++ script.js
@@ -0,0 +1,36 @@
+"use strict";
+/*
+let age = 30;
+let oldAge = age;
+age = 31;
+
+console.log(age, oldAge);
+
+const me = {
+  name: "kevin",
+  age: 18,
+};
+
+const friend = me;
+friend.age = 27;
+
+console.log(friend);
+console.log(me);
+
+
+let lastName = "Ramos";
+let oldLastName = lastName;
+lastName = "Morris";
+console.log(lastName, oldLastName);
+
+const cierra = {
+  firstName: "Cierra",
+  lastName: "Achay",
+  age: 19,
+};
+
+const cierraCopy = Object.assign({}, cierra);
+
+console.log(cierraCopy);
+// object.assign will not create a deep clone...
+*/
